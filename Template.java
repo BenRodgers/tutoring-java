1b7f7fdb7e69e1906dbc38ff4806dbc4fe94e696
BenRodgers <benjaminarodgers@gmail.com>
2022-03-06 17:09:39 +1100

    Added Template.java file for Harsha

---
+++ Template.java
@@ -0,0 +1,14 @@
+class Template {
+    // public static boolean exampleQuestion1() {
+    //Write your solutions in here
+    // }
+    
+
+    // public static boolean exampleQuestion2() {
+        //Write your solutions in here
+    // }
+
+    public static void main(String[] args) {
+       //Test your answers in here
+    }
+}
